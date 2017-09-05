c3224e5c78d96c09e782b298f343983a1a8fcf7b
xiuxiu <ytxiuxiu@gmail.com>
2017-09-05 14:15:48 +1000

    Remove unnecessary dependencies

--- Podfile
+++ Podfile
@@ -3,9 +3,7 @@ platform :ios, '10.0'
 use_frameworks!
 
 target 'restaurant-locator' do
-    pod 'Alamofire', '~> 4.4'
     pod 'SearchTextField'
-    pod 'SwiftyJSON'
     pod 'Cosmos', '~> 11.0'
     pod 'SwiftValidator', :git => 'https://github.com/jpotts18/SwiftValidator.git', :branch => 'master'
     pod 'Whisper'
--- Pods/Target Support Files/Pods-restaurant-locator/Pods-restaurant-locator-frameworks.sh
+++ Pods/Target Support Files/Pods-restaurant-locator/Pods-restaurant-locator-frameworks.sh
@@ -102,19 +102,15 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Cosmos/Cosmos.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SearchTextField/SearchTextField.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftValidator/SwiftValidator.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Whisper/Whisper.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Cosmos/Cosmos.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SearchTextField/SearchTextField.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftValidator/SwiftValidator.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Whisper/Whisper.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
